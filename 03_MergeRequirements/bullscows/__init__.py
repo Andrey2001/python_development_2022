52b19ad038887afae4826e1af68c2b6bedcb8f39
Andrey Kazarinov <a.kazarinov@postgrespro.ru>
2022-03-01 23:55:06 +0300

    add processing of the dictionary file
    also fixed bugs with words length and bulls number

--- 03_MergeRequirements/bullscows/__init__.py
+++ 03_MergeRequirements/bullscows/__init__.py
@@ -7,6 +7,7 @@ import sys
 import random
 import textdistance as txtdist
 import validators
+import os
 import urllib.request
 
 __all__ = ["gameplay", "bullscows"]
@@ -36,18 +37,29 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
         guess_word = ask("Введите слово: ", words)
         b, c = bullscows(guess_word, cur_word)
         inform("Быки: {}, Коровы: {}", b, c)
-        if(b == 4):
+        if(b == len(cur_word)):
             print("Вы выиграли!")
             break
     return steps
 
 def main():
+    length = 5 if len(sys.argv) < 3 else int(sys.argv[2])
     if(validators.url(sys.argv[1])):
         game_dict = urllib.request.urlopen(sys.argv[1])
         game_dict = [i.decode('utf-8')[:-1] for i in game_dict]
-        game_dict = list(filter(lambda x: len(x) == 5, game_dict))
+        game_dict = list(filter(lambda x: len(x) == length, game_dict))
         print(gameplay(ask, inform, game_dict))
-    return 1
+        return 0
+    elif(os.path.isfile("" + sys.argv[1])):
+        with open(sys.argv[1], 'r') as file:
+            game_dict = file.read().split('\n')
+            game_dict = list(filter(lambda x: len(x) == length, game_dict))
+            print(gameplay(ask, inform, game_dict))
+        return 0
+    else:
+        print(os.path.isfile(sys.argv[1]))
+        print("Usage: bullscows DICTIONARY [WORD_LEN]")
+        return 1
 
 if __name__ == '__main__':
     sys.exit(main())
